8541805e9017baaff46cc32e79412d56d91edbbb
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-12-03 23:50:25 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(
+(accumulator,currentValue ) =>accumulator + currentValue,);
+
+//let initialValue = 4;
+//for (const product of batteryBatches)
+
+console.log(totalBatteries);
